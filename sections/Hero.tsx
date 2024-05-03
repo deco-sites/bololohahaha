1d06e1697958d3e45305d2c88baa3a8fdf50e0f2
Miura <vitormiura3@gmail.com>
2024-05-03 12:34:14 +0000

    Release Staging environment with 0 changes made by

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,74 +1,30 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
 import Image from "apps/website/components/Image.tsx";
 
-export interface CTA {
-  id?: string;
-  href: string;
-  text: string;
-  outline?: boolean;
-}
-
 export interface Props {
-  /**
-   * @format rich-text
-   * @default Click here to tweak this text however you want.
-   */
   title?: string;
-  /**
-   * @default This text is fully editable and ready for your personal touch. Just click here, head over to the section window, or dive straight into the code to make changes as you see fit. Whether it's about the content, formatting, font, or anything in between, editing is just a click away.
-   */
   description?: string;
   image?: ImageWidget;
-  placement?: "left" | "right";
-  cta?: CTA[];
 }
 
-const PLACEMENT = {
-  left: "flex-col text-left lg:flex-row-reverse",
-  right: "flex-col text-left lg:flex-row",
-};
-
 export default function HeroFlats({
-  title = "Full Stack Development.",
+  title = "",
   description =
-    "This text is fully editable and ready for your personal touch. Just click here, head over to the section window, or dive straight into the code to make changes as you see fit. Whether it's about the content, formatting, font, or anything in between, editing is just a click away.",
+    "",
   image,
-  placement = "left",
-  cta = [
-    { id: "change-me-1", href: "/", text: "Change me", outline: false },
-    { id: "change-me-2", href: "/", text: "Change me", outline: true },
-  ],
 }: Props) {
   return (
-    <nav class="lg:container lg:mx-auto mx-4">
+    <nav class="lg:mx-auto bg-red-500">
       <div class="flex flex-col items-center gap-8">
         <div
-          class={`flex w-full xl:container xl:mx-auto py-20 mx-5 md:mx-10 z-10 ${
-            image
-              ? PLACEMENT[placement]
-              : "flex-col items-center justify-center text-center"
-          } lg:py-36 gap-12 md:gap-20 items-center`}
+          class="flex w-full xl:container xl:mx-auto py-20 mx-5 md:mx-10 z-10 lg:py-36 gap-12 md:gap-20 items-center"
         >
-          {image && (
-            <Image
-              width={640}
-              class="w-full lg:w-1/2 object-fit"
-              sizes="(max-width: 640px) 100vw, 30vw"
-              src={image}
-              alt={image}
-              decoding="async"
-              loading="lazy"
-            />
-          )}
+
           <div
-            class={`mx-6 lg:mx-auto lg:w-full space-y-4 gap-4 ${
-              image
-                ? "lg:w-1/2 lg:max-w-xl"
-                : "flex flex-col items-center justify-center lg:max-w-3xl"
-            }`}
+            class="mx-6 lg:mx-auto lg:w-full space-y-4 gap-4"
           >
             <div
-              class="inline-block lg:text-[80px] text-4xl leading-none font-medium"
+              class="text-white text-[50px] lg:text-[80px] wonky font-fraunces leading-[50px] lg:leading-[80px]  w-full text-center font-semibold mt-16 md:mt-14 lg:mt-14"
               dangerouslySetInnerHTML={{
                 __html: title,
               }}
@@ -77,20 +33,19 @@ export default function HeroFlats({
             <p class="text-lg md:text-md leading-[150%]">
               {description}
             </p>
-            <div class="flex items-center gap-3">
-              {cta?.map((item) => (
-                <a
-                  key={item?.id}
-                  id={item?.id}
-                  href={item?.href}
-                  target={item?.href.includes("http") ? "_blank" : "_self"}
-                  class={`font-normal btn btn-primary ${
-                    item.outline && "btn-outline"
-                  }`}
-                >
-                  {item?.text}
-                </a>
-              ))}
+            <div className='lg:h-[150px] md:h-[100px] h-[50px]'>
+              <div className='absolute w-full flex justify-center py-[1rem]'>
+                <div className='relative lg:w-[767px] lg:h-[441px] md:w-[514px] md:h-[296px] w-80 h-[184px]'>
+                  {image && (
+                    <Image
+                      src={image}
+                      alt={image}
+                      decoding="async"
+                      loading="lazy"
+                    />
+                  )}
+                </div>
+              </div>
             </div>
           </div>
         </div>
